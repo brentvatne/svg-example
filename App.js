1b29864727c2f55a09851aea588a233558113e1f
Brent Vatne <brentvatne@gmail.com>
2020-07-30 19:20:57 -0700

    example

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { SvgXml } from 'react-native-svg';
+import ReactLogo from './assets/react.svg';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <SvgXml width="200" height="200" xml={ReactLogo} />
       <StatusBar style="auto" />
     </View>
   );
